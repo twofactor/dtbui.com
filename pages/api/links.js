51306164d9dae511d6392c1a146388d57f7e192c
David <davidbui.psd@gmail.com>
2020-06-27 11:37:25 -0700

    no resume

--- pages/api/links.js
+++ pages/api/links.js
@@ -4,7 +4,6 @@ const links = [
   { title: "Home", link: "/" },
   { title: "Twitter", link: "https://twitter.com/ghosttyped" },
   { title: "Github", link: "https://github.com/twofactor" },
-  { title: "Resume", link: "https://www.dropbox.com/s/7gelp5zgfgzvggo/David%20Bui%20Resume%201.1.2b.pdf?dl=0" },
   { title: "Minecraft", link: "https://www.powhattan.org/" },
   { title: "Contact Me", link: "mailto:david@dtbui.com" },
 ];
